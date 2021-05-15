eddb3de570b6364a26a500094b7f76b3b946d3f0
xiaoqing-z <xiaoqingz347@gmail.com>
2021-05-15 13:03:57 -0500

    order by population

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,9 +1,83 @@
+import Link from "next/link";
+import {
+  KeyboardArrowDownRounded,
+  KeyboardArrowUpRounded,
+} from "@material-ui/icons/KeyboardArrowDownRounded";
 import styles from "./CountriesTable.module.css";
+import { useState } from "react";
+
+const orderBy = (countries, direction) => {
+  if (direction === "asc") {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? 1 : -1
+    );
+  }
+
+  if (direction === "desc") {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? -1 : 1
+    );
+  }
+  return countries;
+};
+
+const SortArrow = ({ direction }) => {
+  console.log(direction);
+  if (!direction) {
+    return <></>;
+  }
+
+  if (direction === "desc") {
+    return (
+      <div className={styles.heading_arrow}>
+        <KeyboardArrowDownRounded color="inherit" />
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.heading_arrow}>
+        <KeyboardArrowUpRounded color="inherit" />
+      </div>
+    );
+  }
+};
 
 const CountriesTable = ({ countries }) => {
+  const [direction, setDirection] = useState();
+  const [value, setValue] = useState();
+
+  const orderedCountries = orderBy(countries, "desc");
+
+  const switchDirection = () => {
+    if (!direction) {
+      setDirection("desc");
+    } else if (direction === "desc") {
+      setDirection("asc");
+    } else {
+      setDirection(null);
+    }
+  };
+
   return (
     <div>
-      <div className={styles.heading}></div>
+      <div className={styles.heading}>
+        <button className={styles.heading_name}>
+          <div>Name</div>
+          <SortArrow direction="desc" />
+        </button>
+
+        <button className={styles.heading_population}>
+          <div>Population</div>
+          <SortArrow direction="asc" /* onClick={switchDirection}  */ />
+        </button>
+      </div>
+
+      {orderedCountries.map((country) => (
+        <div className={styles.row}>
+          <div className={styles.name}>{country.name}</div>
+          <div className={styles.population}>{country.population}</div>
+        </div>
+      ))}
     </div>
   );
 };
